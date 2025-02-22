832ffbdcacaa0a4278aaceaba482697ee7b447bd
hasanShaaban <hsahasan16@gmail.com>
2025-02-22 06:11:44 +0300

    splash view logic done

--- lib/core/functions/on_generat_route.dart
+++ lib/core/functions/on_generat_route.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/feature/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:e_commerce/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,9 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
    switch (settings.name){
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
+    
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
    }
--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:e_commerce/core/utils/app_images.dart';
+import 'package:e_commerce/feature/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -22,5 +33,11 @@ class SplashViewBody extends StatelessWidget {
       
       ],
     );
+  }
+  
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), (){
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
