a79b100320ec0d08e15aab8827ad5f839da1f03e
hasanShaaban <hsahasan16@gmail.com>
2025-03-01 08:58:28 +0300

    implement creatUserWithEmailAndPassword function in AuthRepoImpl

---
+++ lib/feature/auth/data/models/user_model.dart
@@ -0,0 +1,13 @@
+import 'package:e_commerce/feature/auth/domain/entity/user_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel extends UserEntity {
+  UserModel({required super.name, required super.email, required super.uId});
+  factory UserModel.fromFirebaseUser(User user) {
+    return UserModel(
+      name: user.displayName ?? '',
+      email: user.email ?? '',
+      uId: user.uid,
+    );
+  }
+}
--- lib/feature/auth/data/repos/auth_repo_impl.dart
+++ lib/feature/auth/data/repos/auth_repo_impl.dart
@@ -1,12 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/exceptions.dart';
 import 'package:e_commerce/core/errors/failures.dart';
+import 'package:e_commerce/core/services/firebase_auth_service.dart';
+import 'package:e_commerce/feature/auth/data/models/user_model.dart';
 import 'package:e_commerce/feature/auth/domain/entity/user_entity.dart';
 import 'package:e_commerce/feature/auth/domain/repos/auth_repo.dart';
 
-class AuthRepoImpl extends AuthRepo{
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthRepoImpl({required this.firebaseAuthService});
   @override
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      var user = await firebaseAuthService.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return left(ServerFailuer(e.message));
+    }catch (e){
+      return left(ServerFailuer('An error occurred, please try again later'));
+    }
   }
 }
